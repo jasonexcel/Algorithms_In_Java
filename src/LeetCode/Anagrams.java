b24963c7b892a430de2fb5cc1da2e65d1435d99a
jasonexcel <jiaxiaoyuan2008@gmail.com>
2014-05-18 20:33:42 -0400

    leetcode/anagrams

---
+++ src/LeetCode/Anagrams.java
@@ -0,0 +1,42 @@
+package LeetCode;
+
+import java.util.*;
+public class Anagrams {
+    public ArrayList<String> anagrams(String[] strs) {
+    	if(strs == null || strs.length == 0){
+    		return null;
+    	}
+    	ArrayList<String> result = new ArrayList<String>();
+    	HashMap<String, ArrayList<String>> hm = new HashMap<String, ArrayList<String>>();
+    	for(String str : strs){
+    		if(str == null){
+    			continue;
+    		}
+    		String sortedStr = sortString(str);
+    		if(hm.containsKey(sortedStr)){
+    			ArrayList<String> temp = hm.get(sortedStr);
+    			temp.add(str);
+    		}
+    		else{
+    			ArrayList<String> temp = new ArrayList<String>();
+    			temp.add(str);
+    			hm.put(sortedStr, temp);
+    		}
+    	}    	
+    	    	
+    	for(ArrayList<String> arrList : hm.values()){
+    		if(arrList.size()>1){
+    			for(String str : arrList){
+    				result.add(str);
+    			}
+    		}
+    	}
+    	return result;
+    }
+    
+    public String sortString(String str){
+    	char[] arr = str.toCharArray();
+    	Arrays.sort(arr);
+    	return Arrays.toString(arr);
+    }
+}
