8617a376c3bdce79c69b2086d8b9ad22a995e49c
Shaun Jia <jiaxiaoyuan2008@gmail.com>
2014-12-15 23:43:07 -0500

    little change

--- src/LeetCode/DistinctSubsequences.java
+++ src/LeetCode/DistinctSubsequences.java
@@ -23,46 +23,48 @@ public class DistinctSubsequences {
 		// TODO Auto-generated method stub
 
 	}
-	  public int numDistinct1(String S, String T) {
-		    // Start typing your Java solution below
-		    // DO NOT write main() function
-		    int[][] dp = new int[T.length() + 1][S.length() + 1];
-		    dp[0][0] = 1;
-		    for (int i = 1; i <= T.length(); i++) {
-		      dp[i][0] = 0;
-		    }
-		    for (int j = 1; j <= S.length(); j++) {
-		      dp[0][j] = 1;
-		    }
-		    for (int i = 1; i <= T.length(); i++) {
-		      for (int j = 1; j <= S.length(); j++) {
-		        dp[i][j] = dp[i][j - 1];
-		        if (T.charAt(i - 1) == S.charAt(j - 1)) {
-		          dp[i][j] += dp[i - 1][j - 1];
-		        }
-		      }
-		    }
-		    return dp[T.length()][S.length()];
-		      
-		  }
-	  
-	  public int numDistinct(String S, String T) {
-		    if(T.length()==0)
-		    {
-		        return 1;
-		    }
-		    if(S.length()==0)
-		        return 0;
-		    int[] res = new int[T.length()+1];
-		    res[0] = 1;
-		    for(int i=0;i<S.length();i++)
-		    {
-		        for(int j=T.length()-1;j>=0;j--)
-		        {
-		            res[j+1] = (S.charAt(i)==T.charAt(j)?res[j]:0)+res[j+1];
-		        }
-		    }
-		    return res[T.length()];
+
+	// easier to understand, O(m*n)
+	public int numDistinct1(String S, String T) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		int[][] dp = new int[T.length() + 1][S.length() + 1];
+		dp[0][0] = 1;
+		// S is empty
+		for (int i = 1; i <= T.length(); i++) {
+			dp[i][0] = 0;
 		}
+		// T is empty
+		for (int j = 1; j <= S.length(); j++) {
+			dp[0][j] = 1;
+		}
+		for (int i = 1; i <= T.length(); i++) {
+			for (int j = 1; j <= S.length(); j++) {
+				if (T.charAt(i - 1) != S.charAt(j - 1)) {
+					dp[i][j] = dp[i][j - 1];
+				} else {
+					dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1];
+				}
+			}
+		}
+		return dp[T.length()][S.length()];
+	}
+
+	public int numDistinct(String S, String T) {
+		if (T.length() == 0) {
+			return 1;
+		}
+		if (S.length() == 0)
+			return 0;
+		int[] res = new int[T.length() + 1];
+		res[0] = 1;
+		for (int i = 0; i < S.length(); i++) {
+			for (int j = T.length() - 1; j >= 0; j--) {
+				res[j + 1] = (S.charAt(i) == T.charAt(j) ? res[j] : 0)
+						+ res[j + 1];
+			}
+		}
+		return res[T.length()];
+	}
 
 }
