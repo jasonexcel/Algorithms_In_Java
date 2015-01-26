77a8b17e675fc6da238f445a932e637aff005d8a
Shaun Jia <jiaxiaoyuan2008@gmail.com>
2015-01-25 23:57:37 -0500

    Arrays & binary method

---
+++ src/LeetCode/SortColors.java
@@ -0,0 +1,57 @@
+/**
+ * Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Note:
+You are not suppose to use the library's sort function for this problem.
+Follow up:
+A rather straight forward solution is a two-pass algorithm using counting sort.
+First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
+
+Could you come up with an one-pass algorithm using only constant space?
+ */
+package LeetCode;
+
+public class SortColors {
+
+	/**
+	 * Time: o(n); space: o(1)
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SortColors ins = new SortColors();
+		int[] A = {1,2};
+		ins.sortColors(A);
+		//System.out.print(A);
+	}
+    public void sortColors(int[] A) {
+        if(A == null || A.length == 0){
+        	return;
+        }
+        //leftP points to where 0 should be filled, rightP points to where 2 should be filled
+        int leftP = 0, rightP = A.length-1;
+        int i = 0;
+        while(i <= rightP){
+        	if(A[i] == 0){
+        		//swap A[i] with A[leftP]
+        		A[i] = A[leftP]; //A[leftP] must be 1
+        		A[leftP] = 0;
+        		leftP++;
+        		i++; //!!!since A[i] = 1, so go to next one
+        	}
+        	else if(A[i] == 2){  
+        		//swap A[i] with A[rightP]
+        		A[i] = A[rightP];
+        		A[rightP] = 2;
+    			rightP--; 
+    			//since A[rightP] can be any value, don't move i
+        	}
+        	else{
+            	i++;
+        	}
+        }
+        return;
+    }
+
+}
---
+++ src/LeetCode/SqrtX.java
@@ -0,0 +1,62 @@
+/**
+ * Implement int sqrt(int x).
+
+Compute and return the square root of x.
+ */
+package LeetCode;
+
+/**
+ * @author Shaun
+ *
+ */
+public class SqrtX {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SqrtX ins = new SqrtX();
+		int res = ins.sqrt(7);
+		System.out.print(res);
+	}
+    public int sqrt(int x) {
+        if(x <= 0){
+        	return 0;
+        }
+        int min = 1;
+        int max = x/2+1;
+        while(min<=max){
+        	int mid = (min+max)/2;
+        	//use division not multiplication
+        	if(x/mid>mid){
+        		if(x/(mid+1)<(mid+1)){
+        			return mid;
+        		}
+        		else{
+        			min = mid + 1;
+        		}
+        	}
+        	else if(x/mid<mid){
+        		max = mid - 1;
+        	}
+        	else{
+        		return mid;
+        	}
+        }
+        return max;
+    }
+    
+    //newton method: equation a[n+1]=(a[n]+x/a[n])/2
+    public int sqrtII(int x) {
+        if (x == 0) return 0;
+        double lastY = 0;
+        double y = 1;
+        while (y != lastY)
+        {
+            lastY = y;
+            y = (y + x / y) / 2;
+        }
+        return (int)(y);
+    }
+}
