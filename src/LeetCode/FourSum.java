39a6110ee00e296e0f88aaf10251767edddcecf6
Shaun Jia <jiaxiaoyuan2008@gmail.com>
2015-02-17 00:42:57 -0500

    sums

--- src/LeetCode/FourSum.java
+++ src/LeetCode/FourSum.java
@@ -13,6 +13,10 @@ The solution set must not contain duplicate quadruplets.
  */
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 /**
  * @author Shaun
  *
@@ -26,5 +30,112 @@ public class FourSum {
 		// TODO Auto-generated method stub
 
 	}
-
+	//use hashset to resolve duplicates, easiest to code
+	//o(n^3) time complexity, o(1) space (extra space)
+	public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
+	    HashSet<ArrayList<Integer>> hashSet = new HashSet<ArrayList<Integer>>();
+	    ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+	    Arrays.sort(num);
+	    for (int i = 0; i <= num.length-4; i++) {
+	        for (int j = i + 1; j <= num.length-3; j++) {
+	            int low = j + 1;
+	            int high = num.length - 1;
+	 
+	            while (low < high) {
+	                int sum = num[i] + num[j] + num[low] + num[high];
+	 
+	                if (sum > target) {
+	                    high--;
+	                } else if (sum < target) {
+	                    low++;
+	                } else if (sum == target) {
+	                    ArrayList<Integer> temp = new ArrayList<Integer>();
+	                    temp.add(num[i]);
+	                    temp.add(num[j]);
+	                    temp.add(num[low]);
+	                    temp.add(num[high]);
+	 
+	                    if (!hashSet.contains(temp)) {
+	                        hashSet.add(temp);
+	                        result.add(temp);
+	                    }
+	 
+	                    low++;
+	                    high--;
+	                }
+	            }
+	        }
+	    }
+	 
+	    return result;
+	}
+	
+	//complete method
+	public ArrayList<ArrayList<Integer>> fourSumCodeGanker(int[] num, int target) {
+	    ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+	    if(num==null||num.length==0)
+	        return res;
+	    Arrays.sort(num);
+	    for(int i=num.length-1;i>2;i--)
+	    {
+	        if(i==num.length-1 || num[i]!=num[i+1])
+	        {
+	            ArrayList<ArrayList<Integer>> curRes = threeSum(num,i-1,target-num[i]);
+	            for(int j=0;j<curRes.size();j++)
+	            {
+	                curRes.get(j).add(num[i]);
+	            }
+	            res.addAll(curRes);
+	        }
+	    }
+	    return res;        
+	}
+	private ArrayList<ArrayList<Integer>> threeSum(int[] num, int end, int target)
+	{
+	    ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+	    for(int i=end;i>1;i--)
+	    {
+	        if(i==end || num[i]!=num[i+1])
+	        {
+	            ArrayList<ArrayList<Integer>> curRes = twoSum(num,i-1,target-num[i]);
+	            for(int j=0;j<curRes.size();j++)
+	            {
+	                curRes.get(j).add(num[i]);
+	            }
+	            res.addAll(curRes);
+	        }
+	    }
+	    return res;
+	}
+	private ArrayList<ArrayList<Integer>> twoSum(int[] num, int end, int target)
+	{
+	    ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+	    int l=0;
+	    int r=end;
+	    while(l<r)
+	    {
+	        if(num[l]+num[r]==target)
+	        {
+	            ArrayList<Integer> item = new ArrayList<Integer>();
+	            item.add(num[l]);
+	            item.add(num[r]);
+	            res.add(item);
+	            l++;
+	            r--;
+	            while(l<r&&num[l]==num[l-1])
+	                l++;
+	            while(l<r&&num[r]==num[r+1])
+	                r--;
+	        }
+	        else if(num[l]+num[r]>target)
+	        {
+	            r--;
+	        }
+	        else
+	        {
+	            l++;
+	        }
+	    }
+	    return res;
+	}
 }
--- src/LeetCode/ThreeSumClosest.java
+++ src/LeetCode/ThreeSumClosest.java
@@ -1,3 +1,11 @@
+/**
+ * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target.
+ * Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+    For example, given array S = {-1 2 1 -4}, and target = 1.
+
+    The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ */
 package LeetCode;
 import java.util.Arrays;
 public class ThreeSumClosest {
@@ -9,12 +17,13 @@ public class ThreeSumClosest {
 		// TODO Auto-generated method stub
 
 	}
+	//time complexity o(n^2), space o(n)
 	 public int threeSumClosest(int[] num, int target) {
 	        // Start typing your Java solution below
 	        // DO NOT write main() function
 	        int len = num.length;
 	        if(len<3) return -1;
-	        int close = num[0] + num[1] + num[len-1];
+	        int close = num[0] + num[1] + num[len-1];//initialization
 	        int sum;
 	        Arrays.sort(num);
 	        for(int i = 0; i<len-2; i++){
