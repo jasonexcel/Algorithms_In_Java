e4cb09e3b977be5352235b349ebd0ee43f2455f0
Shaun Jia <jiaxiaoyuan2008@gmail.com>
2015-02-19 00:34:52 -0500

    numerical

---
+++ src/LeetCode/MaxPointsOnALine.java
@@ -0,0 +1,20 @@
+/**
+ * Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+ */
+package LeetCode;
+
+/**
+ * @author Shaun
+ *
+ */
+public class MaxPointsOnALine {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
