0546d0e1cbd3a14a21b2b2608f545f70a03c2f6f
Shaun Jia <jiaxiaoyuan2008@gmail.com>
2015-05-14 00:31:27 -0400

    z

--- src/Zcode/Z_code/ThreeSumNotGreaterThanVal.java
+++ src/Zcode/Z_code/ThreeSumNotGreaterThanVal.java
@@ -1,21 +1,16 @@
 /**
- * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
- *  Find all unique triplets in the array which gives the sum of zero.
-
-Note:
-Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
-The solution set must not contain duplicate triplets.
-    For example, given array S = {-1 0 1 2 -1 -4},
-
-    A solution set is:
-    (-1, 0, 1)
-    (-1, -1, 2)
+ * 
  */
+package Zcode.Z_code;
 
-package LeetCode;
 import java.util.ArrayList;
 import java.util.Arrays;
-public class ThreeSum {
+
+/**
+ * @author Shaun
+ *
+ */
+public class ThreeSumNotGreaterThanVal {
 
 	/**
 	 * @param args
@@ -24,8 +19,8 @@ public class ThreeSum {
 		// TODO Auto-generated method stub
 
 	}
-	
-    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+	// binary search to find the upperbound
+    public ArrayList<ArrayList<Integer>> threeSumNoGreater(int[] num, int val) {
         // Start typing your Java solution below
         // DO NOT write main() function
         ArrayList<ArrayList<Integer>> arrays = new ArrayList<ArrayList<Integer>>();
@@ -38,26 +33,25 @@ public class ThreeSum {
             int j = i+1;
             int k = num.length-1;
             while(j<k) {
-                if(num[i] + num[j] + num[k] == 0) {
+                if(num[i] + num[j] + num[k] < val) {
                     arrays.add(new ArrayList<Integer>(Arrays.asList(num[i], num[j], num[k])));
                     j++; // increment first
-                    k--;
                     while(j<k && num[j] == num[j-1]){
                     	j++; //skip same values
                     }
-                    while(j<k &&num[k]==num[k+1]){
-                    	k--; //skip save values
-                    }
-                }
-                else if(num[i] + num[j] + num[k] > 0) {
-                    k--;
+
                 }
                 else {
-                    j++;
+                	if(num[i] + num[j] + num[k] == val) {
+                    	arrays.add(new ArrayList<Integer>(Arrays.asList(num[i], num[j], num[k])));
+                    }
+                	k--;
+                    while(j<k && num[k]==num[k+1]){
+                    	k--; //skip same values
+                    }
                 }
             }
         }
         return arrays;
 	}
-
 }
---
+++ src/Zcode/Z_code/WinningAtZero.java
@@ -0,0 +1,23 @@
+package Zcode.Z_code;
+
+/**
+ * Given array of whole numbers and a starting index, find out if you can win
+Winning means ending up at an array value of 0
+If you are at a position that has a value that is not a 0, you can move value times to the left or to the right
+[1, 3, 0, 2, 2], i == 0 -> true
+[1, 1], i == 1 -> false. visit 1point3acres.com for more.
+
+ * @author Shaun
+ *
+ */
+public class WinningAtZero {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
